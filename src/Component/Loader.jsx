5197f0474e36da2b88a16d1dc4c0dca61f9260ea
Akshay-singh-goyal <152581106+Akshay-singh-goyal@users.noreply.github.com>
2025-10-17 13:30:13 +0530

    Update Loader.jsx

--- src/Component/Loader.jsx
+++ src/Component/Loader.jsx
@@ -1,58 +1,17 @@
-// src/Component/Loader.jsx
 import React from "react";
-import { Box, CircularProgress, Typography } from "@mui/material";
-import utsavLogo from "./Images/utsavlogo.png";
+import "./Loader.css";
+import { CircularProgress, Typography } from "@mui/material";
 
 const Loader = () => {
   return (
-    <Box
-      sx={{
-        height: "100vh",
-        width: "100vw",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundImage: `url(${utsavLogo})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        position: "relative",
-      }}
-    >
-      {/* Overlay for better visibility of loader */}
-      <Box
-        sx={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          height: "100%",
-          width: "100%",
-          backgroundColor: "rgba(0, 0, 0, 0.5)", // semi-transparent dark overlay
-          zIndex: 1,
-        }}
-      />
-
-      {/* Loader and text */}
-      <Box
-        sx={{
-          position: "relative",
-          zIndex: 2,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
+    <div className="loader-container">
+      <div className="loader-box">
         <CircularProgress size={70} thickness={5} color="primary" />
-        <Typography
-          variant="h6"
-          mt={2}
-          sx={{ color: "#fff", fontWeight: "bold", textAlign: "center" }}
-        >
-          Loading, please wait...
+        <Typography className="loader-text">
+         Welcome to ustav-aura Loading, please wait...
         </Typography>
-      </Box>
-    </Box>
+      </div>
+    </div>
   );
 };
 
